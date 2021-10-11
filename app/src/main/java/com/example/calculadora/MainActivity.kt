68862e4ea6bf1361772131c149d2920078223390
LCasagrandi <leonardocsgr@gmail.com>
2021-10-11 15:55:39 -0300

    Adição dos elementos & view binding

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -2,15 +2,14 @@ package com.example.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.calculadora.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = MainActivity.inflate(layoutInflater)
-
-        binding.title.text = "Hello"
-        binding.subtext.text = "Concise, safe code"
-        binding.button.setOnClickListener { /* ... */ }
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
     }
+}
